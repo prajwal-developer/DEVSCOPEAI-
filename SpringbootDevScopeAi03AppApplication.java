c31f15d8b85cc7473c1512946daa3040c4b3f182
Prajwal TechDev <pmambulgi@gmail.com>
2026-04-19 11:55:15 +0530

    Add files via upload

---
+++ SpringbootDevScopeAi03AppApplication.java
@@ -0,0 +1,13 @@
+package com.kodnest.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevScopeAi03AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevScopeAi03AppApplication.class, args);
+	}
+
+}
